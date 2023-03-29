500082254b36e26eb1921bba0027181f0fabaec3
solloball <109233909+solloball@users.noreply.github.com>
2023-03-29 10:26:17 +0700

    made 32x32 display

--- helloworld.asm
+++ helloworld.asm
@@ -1,22 +1,34 @@
-asect 0xc0
-display:
-
 asect 0x00
 
 ldi r0, display
-ldi r1, 0xff
-
-ldi r2, 33
+ldi r2, 128
+ldi r3, end_display
 
 while
-    dec r2
-stays nz
-    st r0, r1
+    cmp r3, r0
+stays pl 
+    ldi r1, 96
+    while
+        cmp r1, r2
+    stays ne
+        st r0, r1
+        inc r1
+    wend
     inc r0
 wend
 
 
 halt
 
+asect 0xc0
+display:
+ds 32
+
+asect 0xe0
+end_display:
+
+asect 0xe1
+joystick:
+ds 1
 
 end
--- tess.asm
+++ tess.asm
@@ -1,14 +1,33 @@
-asect 0xc0
-display:
-
 asect 0x00
 
 ldi r0, display
-ldi r1, 0xaa
+ldi r2, 64
+ldi r3, end_display
 
-st r0, r1
-
-ld r0, r2
+while
+    cmp r3, r0
+stays pl 
+    ldi r1, 32
+    while
+        cmp r1, r2
+    stays ne
+        st r0, r1
+        inc r1
+    wend
+    inc r0
+wend
 
 halt
+
+asect 0xc0
+display:
+ds 32
+
+asect 0xe0
+end_display:
+
+asect 0xe1
+joystick:
+ds 1
+
 end
