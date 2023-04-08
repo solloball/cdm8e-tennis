d918f335126a127d3910731388d80bbea992a964
Kirill <k.romanov1@g.nsu.ru>
2023-04-08 23:12:49 +0700

    added updrage and reflect

--- tess.asm
+++ tess.asm
@@ -1,42 +1,90 @@
 asect 0x48
 
-    ldi r0, score_left
-    ldi r1, 3
-    st r0, r1
-    ld r0, r2
-    ldi r1, 5
+    ldi r1, xBall
+    ldi r2, 10 #set coord x to xBalls
+    st r1, r2
+
+    ldi r2, 10 #set coord y to yBalls
+    ldi r1, yBall
+    st r1, r2
+
+
+    ldi r0, V
+    ldi r1, 0x77 # set value of speed ( правые четыре биты отводятся под занчение скорости для координаты Y, которые хранятся в дополненном коде, 4 правых бита под значения скорости X)
     st r0, r1
 
 
-    ldi r0, score_right
-    ldi r1, 8
-    st r0, r1
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3 # происходит увеличение координат, и в данном случае r3 записывается значение коордианты x для удобства в валидной форме ( то есть правые 3 бита нулевые, правые 5 бит  - само значение)
+
+    jsr draw_pixel
+    ldi r0, update
     ld r0, r3
-    ldi r1, 7
-    st r0, r1
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+
+    ldi r0, reflectX
+    st r0, r1 # при таком вызове значение x будет меняться на противоположное, что кидать вторым регистром не имеет значения (то есть отражение :)
+
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+
+    ldi r0, reflectY
+    st r0, r1 # то же самое но со скростью y
+
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+    jsr draw_pixel
+    ldi r0, update
+    ld r0, r3
+
 
 halt
 
 draw_pixel:
     ldi r0, display
     ldi r1,xBall
+    ldi r3, 0x1f
     ld r1, r1
+    and r3, r1 
     add r1, r0
     ldi r1, 0x20 #write ball
     ldi r2, yBall
     ld r2, r2
+    and r3, r2 
     or r2, r1
     st r0, r1
-    rts
-
-go_up:
-    jsr inc_yBall
-    jsr draw_pixel
-    rts
-
-go_down:
-    jsr dec_yBall
-    jsr draw_pixel
     rts
 
 clr_pixel:
@@ -48,135 +96,7 @@ clr_pixel:
     st r0, r1 
     rts
 
-go_right:
-    jsr clr_pixel
-    jsr inc_xBall
-    jsr draw_pixel
-    rts
-
-go_left:
-    jsr clr_pixel
-    jsr dec_xBall
-    jsr draw_pixel
-    rts
-
-go_up_right:
-    jsr clr_pixel
-    jsr inc_xBall
-    jsr inc_yBall
-    jsr draw_pixel
-    rts
-
-go_up_left:
-    jsr clr_pixel
-    jsr dec_xBall
-    jsr inc_yBall
-    jsr draw_pixel
-    rts
-
-go_down_left:
-    jsr clr_pixel
-    jsr dec_xBall
-    jsr dec_yBall
-    jsr draw_pixel
-    rts
-
-go_down_right:
-    jsr clr_pixel
-    jsr inc_xBall
-    jsr dec_yBall
-    jsr draw_pixel
-    rts
-
-inc_xBall:
-    ldi r0, xBall
-    ld r0, r1
-    inc r1
-    st r0, r1
-    rts
-
-dec_xBall:
-    ldi r0, xBall
-    ld r0, r1
-    dec r1
-    st r0, r1
-    rts
-
-inc_yBall:
-    ldi r0, yBall
-    ld r0, r1
-    if
-        ldi r2, 0x7f #top_display
-        cmp r2, r1
-    is ne
-        inc r1
-        st r0, r1
-    fi #иначе...нужно прописать логику отскока
-    rts
-
-dec_yBall:
-    ldi r0, yBall
-    ld r0, r1
-    if
-        ldi r2, 0x5e #bottom_display
-        cmp r2, r1
-    is ne
-        dec r1
-        st r0, r1
-    fi #иначе...нужно прописать логику отскока
-    rts
-
-go_up_pl_left:
-    ldi r0, display
-    ld r0, r1
-    if
-    ldi r2, 0x7d #top_display - 2
-        cmp r1, r2
-    is ne
-        inc r1
-        st r0, r1
-    fi
-    rts
-
-go_down_pl_left:
-    ldi r0, display
-    ld r0, r1
-    if
-        ldi r2, 0x60 #bottom_display + 2
-        cmp r2, r1
-    is ne
-        dec r1
-        st r0, r1
-    fi
-    rts
-
-go_up_pl_right:
-    ldi r0, end_display
-    ld r0, r1
-    if
-    ldi r2, 0x7d #top_display - 2
-        cmp r1, r2
-    is ne
-        inc r1
-        st r0, r1
-    fi
-    rts
-
-go_down_pl_right:
-    ldi r0, end_display
-    ld r0, r1
-    if
-        ldi r2, 0x60 #bottom_display + 2
-        cmp r2, r1
-    is ne
-        dec r1
-        st r0, r1
-    fi
-    rts
-
 asect 0x40
-xBall: ds 1
-yBall: ds 1
 yPlatform1: ds 1
 yPlatform2: ds 1
 top_display: dc 0x7f
@@ -200,6 +120,31 @@ ds 1
 
 asect 0x22
 score_right:
+ds 1
+
+asect 0x23
+V:
+ds 1
+
+asect 0x24
+xBall:
+ds 1
+
+asect 0x25
+yBall:
+ds 1
+
+
+asect 0x26
+update:
+ds 1
+
+asect 0x27
+reflectX:
+ds 1
+
+asect 0x28
+reflectY:
 ds 1
 
 end
